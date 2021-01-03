1e6b1e0cffb985ac217cef88f85ec2ba1e719806
ldir92664 <ldir92664@163.com>
2021-01-03 15:56:23 +0800

    Add files via upload

---
+++ CLOCK-and-meteostation-on-ESP8266-Max7219-V4-UA-master/MOD_光敏电阻/CLOCK_ESP8266/CLOCK_ESP8266_mod_v22/langUA.h
@@ -0,0 +1,50 @@
+﻿
+/*
+ * Украинская локализация mod by v22 
+ * 
+ */
+
+//  локализация основного кода
+ 
+String L_today = "Сьогодні";
+String L_outdoor = "На дворі зара";
+String L_Humidity = "Вологість";
+String L_Atmospheric = "Атмосферний тиск";
+String L_Atmospheric_mm = "мм";
+String L_Cloudiness = "Хмарність";
+String L_Wind = "Вітер";
+String L_Wind_Northern = "Північний";
+String L_Wind_Northeastern = "Північно-східний";
+String L_Wind_East = "Східний";
+String L_Wind_Southeastern = "Південно-східний";
+String L_Wind_Southern = "Південний";
+String L_Wind_Southwestern = "Південно-західний";
+String L_Wind_West = "Західний";
+String L_Wind_Northwestern = "Північно-західний";
+String L_Windspeed = "м/с";
+String L_tomorrow = "Завтра температура";
+String L_mon_January = "січня";
+String L_mon_February = "лютого";
+String L_mon_March = "березня";
+String L_mon_April = "квітня";
+String L_mon_May = "травня";
+String L_mon_June = "червня";
+String L_mon_July = "липня";
+String L_mon_August = "серпня";
+String L_mon_September = "вересня";
+String L_mon_October = "жовтня";
+String L_mon_November = "листопада";
+String L_mon_December = "грудня";
+String L_WD_Monday = "Понеділок";
+String L_WD_Tuesday = "Вівторок";
+String L_WD_Wednesday = "Середа";
+String L_WD_Thursday = "Четвер";
+String L_WD_Friday = "П'ятниця";
+String L_WD_Saturday = "Субота";
+String L_WD_Sunday = "Неділя";
+String L_No_conn = "Вiдсутне пiдключення до WIFI. Під'єднайтися до WiFi-Clock-v4  та наберiть у браузерi 192.168.4.1";
+String L_YoursIP = "Ваш IP:";
+// Відсутній переклад на сайті зі станом погоди 
+String L_weatherDescription_shower_sleet = "дощ зi снiгом";
+String L_weatherDescription_light_shower_snow = "слабий снігопад";
+//  локализация HTMLок
